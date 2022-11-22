d4c76755fd77be5d7463d449f419e400f3236cac
Rustam <antinov.rus@mail.ru>
2022-11-22 21:35:23 +0700

    Delete lines from main activity

--- app/src/main/java/com/example/forgittry/MainActivity.kt
+++ app/src/main/java/com/example/forgittry/MainActivity.kt
@@ -7,7 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("second commit")
-        print("second second commit")
     }
 }
