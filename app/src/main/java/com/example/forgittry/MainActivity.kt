c2a1f3d6f1bdea169b8e839462011d1f221c5209
Rustam <antinov.rus@mail.ru>
2022-11-22 21:33:17 +0700

    Add all files

--- app/src/main/java/com/example/forgittry/MainActivity.kt
+++ app/src/main/java/com/example/forgittry/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("second commit")
+        print("second second commit")
     }
 }
